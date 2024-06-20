0f8ff25dfff3e13a5ff7260c73772a5177e0e918
Andrew González <andrewgonzalezcr7@gmail.com>
2024-06-20 17:59:22 -0400

    Added the changes to achieve the profile card functionality in plain html/css

---
+++ js/index.js
@@ -0,0 +1,12 @@
+// Write custom JavaScript here.
+// You may ignore this file and delete if if JavaScript is not required for your challenge.
+
+// var button = document.getElementsByClassName("btn-blue")[0];
+
+// button.addEventListener("mouseenter", (event)=>{
+//     event.target.classList.add('bg-blue-700');
+// }, false,);
+
+// button.addEventListener("mouseleave", (event)=>{
+//     event.target.classList.remove('bg-blue-700');
+// }, false,);
